6e5107beb8e972af8af2ba5078a48002a1abbc6f
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2016-06-25 15:47:36 -0500

    add basic documentation to $mdExpansionPanel

--- src/js/expansionPanel.service.js
+++ src/js/expansionPanel.service.js
@@ -3,6 +3,20 @@ angular
   .factory('$mdExpansionPanel', expansionPanelService);
 
 
+/**
+ * @ngdoc service
+ * @name $mdExpansionPanel
+ * @module material.components.expansionPanels
+ *
+ * @description
+ * Expand and collapse Expansion Panel using its `md-component-id`
+ *
+ * @example
+ * $mdExpansionPanel('comonentId').then(function (instance) {
+ *  instance.exapand();
+ *  instance.collapse();
+ * });
+ */
 expansionPanelService.$inject = ['$mdComponentRegistry'];
 function expansionPanelService($mdComponentRegistry) {
   return function (handle) {
