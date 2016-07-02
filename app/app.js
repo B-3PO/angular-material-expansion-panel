c1cd958c025d70f0160a1e974af35c062a232d0e
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2016-07-02 00:08:26 -0500

    updated test app adding multiple examples

--- app/app.js
+++ app/app.js
@@ -11,8 +11,23 @@ configApp.$inject = ['$routeProvider'];
 function configApp($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'partials/home.html',
+      templateUrl: 'pages/home/home.html',
       controller: 'HomeController',
+      controllerAs: 'vm'
+    })
+    .when('/group', {
+      templateUrl: 'pages/group/group.html',
+      controller: 'GroupController',
+      controllerAs: 'vm'
+    })
+    .when('/autoexpand', {
+      templateUrl: 'pages/autoExpand/autoExpand.html',
+      controller: 'AutoExpandController',
+      controllerAs: 'vm'
+    })
+    .when('/multiple', {
+      templateUrl: 'pages/multiple/multiple.html',
+      controller: 'MultipleController',
       controllerAs: 'vm'
     })
     .otherwise('/');
