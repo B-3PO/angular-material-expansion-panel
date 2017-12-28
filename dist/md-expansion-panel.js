135952acb2d02bd60d29dc5e9a06adcbd2a57b4f
Chen Lijun <chenlijun1999@gmail.com>
2017-12-29 00:38:34 +0100

    Update builds and bump version

--- dist/md-expansion-panel.js
+++ dist/md-expansion-panel.js
@@ -37,7 +37,7 @@ function expansionPanelDirective() {
     require: ['mdExpansionPanel', '?^^mdExpansionPanelGroup'],
     scope: true,
     compile: compile,
-    controller: ['$scope', '$element', '$attrs', '$window', '$$rAF', '$mdConstant', '$mdUtil', '$mdComponentRegistry', '$timeout', '$q', '$animate', '$parse', controller]
+    controller: ['$scope', '$element', '$attrs', '$window', '$$rAF', '$mdConstant', '$mdUtil', '$mdComponentRegistry', '$timeout', '$q', '$animate', '$parse', '$compile', controller]
   };
   return directive;
 
@@ -68,7 +68,7 @@ function expansionPanelDirective() {
 
 
 
-  function controller($scope, $element, $attrs, $window, $$rAF, $mdConstant, $mdUtil, $mdComponentRegistry, $timeout, $q, $animate, $parse) {
+  function controller($scope, $element, $attrs, $window, $$rAF, $mdConstant, $mdUtil, $mdComponentRegistry, $timeout, $q, $animate, $parse, $compile) {
     /* jshint validthis: true */
     var vm = this;
 
@@ -232,7 +232,7 @@ function expansionPanelDirective() {
       options = options || {};
 
       var deferred = $q.defer();
-        $scope.$emit("mdExpansionPanelExpanding", vm.componentId);
+      $scope.$emit("mdExpansionPanelExpanding", vm.componentId);
 
       if (vm.epxansionPanelGroupCtrl) {
         vm.epxansionPanelGroupCtrl.expandPanel(vm.componentId);
@@ -248,6 +248,13 @@ function expansionPanelDirective() {
 
       initEvents();
       collapsedCtrl.hide(options);
+
+      if (expandedCtrl.innerTemplate) {
+        expandedCtrl.$element.append(expandedCtrl.innerTemplate);
+        $compile(expandedCtrl.$element.children())($scope);
+        // set to null to that it gets compile only once
+        expandedCtrl.innerTemplate = null;
+      }
       expandedCtrl.show(options);
 
       if (headerCtrl) { headerCtrl.show(options); }
@@ -642,79 +649,94 @@ function expansionPanelCollapsedDirective($animateCss, $timeout) {
  **/
 expansionPanelExpandedDirective.$inject = ['$animateCss', '$timeout'];
 function expansionPanelExpandedDirective($animateCss, $timeout) {
+
   var directive = {
     restrict: 'E',
     require: '^^mdExpansionPanel',
-    link: link
+    compile: compile
   };
   return directive;
 
+  function compile(element, attrs) {
+    var innerTemplate;
 
-  function link(scope, element, attrs, expansionPanelCtrl) {
-    var setHeight = attrs.height || undefined;
-    if (setHeight !== undefined) { setHeight = setHeight.replace('px', '') + 'px'; }
-
-    expansionPanelCtrl.registerExpanded({
-      show: show,
-      hide: hide,
-      setHeight: setHeight !== undefined,
-      $element: element
-    });
-
-
-
-
-    function hide(options) {
-      var height = setHeight ? setHeight : element[0].scrollHeight + 'px';
-      element.addClass('md-hide md-overflow');
-      element.removeClass('md-show md-scroll-y');
-
-      var animationParams = {
-        from: {'max-height': height, opacity: 1},
-        to: {'max-height': '48px', opacity: 0}
-      };
-      if (options.animation === false) { animationParams.duration = 0; }
-      $animateCss(element, animationParams)
-      .start()
-      .then(function () {
-        element.css('display', 'none');
-        element.removeClass('md-hide');
-      });
+    if (attrs.lazyRender !== undefined) {
+      innerTemplate = element.html();
+      element.empty();
     }
 
+    return link;
 
-    function show(options) {
-      element.css('display', '');
-      element.addClass('md-show md-overflow');
-      // use passed in height or the contents height
-      var height = setHeight ? setHeight : element[0].scrollHeight + 'px';
+    function link(scope, element, attrs, expansionPanelCtrl) {
+      var setHeight = attrs.height || undefined;
+      if (setHeight !== undefined) { setHeight = setHeight.replace('px', '') + 'px'; }
 
-      var animationParams = {
-        from: {'max-height': '48px', opacity: 0},
-        to: {'max-height': height, opacity: 1}
-      };
-      if (options.animation === false) { animationParams.duration = 0; }
-      $animateCss(element, animationParams)
-      .start()
-      .then(function () {
-
-        // if height was passed in then set div to scroll
-        if (setHeight !== undefined) {
-          element.addClass('md-scroll-y');
-        } else {
-          // safari will animate the max-height if transition is not set to 0
-          element.css('transition', 'none');
-          element.css('max-height', 'none');
-          // remove transition block on next digest
-          $timeout(function () {
-            element.css('transition', '');
-          }, 0);
-        }
-
-        element.removeClass('md-overflow');
+      expansionPanelCtrl.registerExpanded({
+        show: show,
+        hide: hide,
+        setHeight: setHeight !== undefined,
+        $element: element,
+        innerTemplate: innerTemplate
       });
+
+
+
+
+      function hide(options) {
+        var height = setHeight ? setHeight : element[0].scrollHeight + 'px';
+        element.addClass('md-hide md-overflow');
+        element.removeClass('md-show md-scroll-y');
+
+        var animationParams = {
+          from: {'max-height': height, opacity: 1},
+          to: {'max-height': '48px', opacity: 0}
+        };
+        if (options.animation === false) { animationParams.duration = 0; }
+        $animateCss(element, animationParams)
+          .start()
+          .then(function () {
+            element.css('display', 'none');
+            element.removeClass('md-hide');
+          });
+      }
+
+
+      function show(options) {
+        element.css('display', '');
+        element.addClass('md-show md-overflow');
+        // use passed in height or the contents height
+        var height = setHeight ? setHeight : element[0].scrollHeight + 'px';
+
+        var animationParams = {
+          from: {'max-height': '48px', opacity: 0},
+          to: {'max-height': height, opacity: 1}
+        };
+        if (options.animation === false) { animationParams.duration = 0; }
+        $animateCss(element, animationParams)
+          .start()
+          .then(function () {
+
+            // if height was passed in then set div to scroll
+            if (setHeight !== undefined) {
+              element.addClass('md-scroll-y');
+            } else {
+              // safari will animate the max-height if transition is not set to 0
+              element.css('transition', 'none');
+              element.css('max-height', 'none');
+              // remove transition block on next digest
+              $timeout(function () {
+                element.css('transition', '');
+              }, 0);
+            }
+
+            element.removeClass('md-overflow');
+          });
+      }
     }
   }
+
+
+
 }
 }());
 (function(){"use strict";angular
